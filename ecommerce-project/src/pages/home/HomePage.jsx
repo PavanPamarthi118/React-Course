a99b68d4f8ae2b0b2cf1c383badec2e083705615
Pavan <74958980+PavanPamarthi118@users.noreply.github.com>
2025-11-22 15:11:54 +0530

    Create add to cart feature

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -4,7 +4,7 @@ import { Header } from '../../components/Header';
 import { ProductsGrid } from './ProductsGrid';
 import './HomePage.css';
 
-export function HomePage({ cart }) {
+export function HomePage({ cart, loadCart }) {
 
     const [products, setProducts] = useState([]);
 
@@ -16,7 +16,7 @@ export function HomePage({ cart }) {
     // }, []);
 
     //async, await
-        useEffect(() => {
+    useEffect(() => {
         const getHomeData = async () => {
             const response = await axios.get("/api/products")
             setProducts(response.data);
@@ -32,7 +32,7 @@ export function HomePage({ cart }) {
             <Header cart={cart} />
 
             <div className="home-page">
-                <ProductsGrid products={products} />
+                <ProductsGrid products={products} cart={cart} loadCart={loadCart} />
             </div>
         </>
 
